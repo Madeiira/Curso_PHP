4d976e96e68bdb00ea6fca7d69ebdd0e7904631e
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-16 20:21:19 -0300

    Adicionando expressões ao programa operadores e adicionando comentarios ao operadores

--- relembrando/operadores.php
+++ relembrando/operadores.php
@@ -1,1 +1,7 @@
 <?php
+
+// Expressão -> instrução de código 
+// EX: Soma, Baskhara, impressão de texto, regras de negocio, login
+
+// Operadores -> Recursos para compor expressões
+// EX: < , > , >= , == , = , + , *, **
