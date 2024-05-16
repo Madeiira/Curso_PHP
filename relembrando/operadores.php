02404fc875d86a38eef8d5003bd46fbe0f8c2277
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-16 20:26:06 -0300

    Adicioando exemplo de ordem de operadores

--- relembrando/operadores.php
+++ relembrando/operadores.php
@@ -5,3 +5,27 @@
 
 // Operadores -> Recursos para compor expressões
 // EX: < , > , >= , == , = , + , *, **
+
+
+// Ordem dos operadores é cfe a matematica
+// 2 + 2 * 6 = 14
+
+function contaDefault($a, $b, $c){
+
+    $contaDefault = $a + $b * $c;
+    echo "Conta default: ". $contaDefault;
+
+}
+
+contaDefault(2,2,6);
+
+echo "<br>";
+
+function contaDif($a, $b, $c){
+
+    $contaDif = ($a + $b) * $c;
+    echo "Conta dif: ". $contaDif;
+
+}
+
+contaDif(2,2,6);
