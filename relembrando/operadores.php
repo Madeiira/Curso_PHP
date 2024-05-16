ce2dd7606be2c0ef36d8435a80e67e6fa043e61e
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-16 20:57:07 -0300

    Adicionando códigos nos operadores

--- relembrando/operadores.php
+++ relembrando/operadores.php
@@ -29,3 +29,29 @@ function contaDif($a, $b, $c){
 }
 
 contaDif(2,2,6);
+echo "<br>";
+
+//AUTOCAST
+
+$var1 = 5.9;
+$var2 = 2;
+
+echo $divisao = ceil($var1 / $var2);
+
+echo "<br>";
+
+echo $divisao = $var1 / $var2;
+
+$expressao = "5" * 12;
+
+echo "<br>";
+
+echo "Resultado: ". $expressao;
+
+echo "<br>";
+
+echo "gettype: ". gettype($expressao);
+
+
+//OPERADOR DE MODULO
+
