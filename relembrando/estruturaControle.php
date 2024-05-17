a8e34e62a33e95f33a0f7c487a104055dfee1ba5
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-17 18:00:51 -0300

    Ajustando if aninhado

--- relembrando/estruturaControle.php
+++ relembrando/estruturaControle.php
@@ -1,22 +1,23 @@
 <?php
 
-function controleIF($var1, $var2, $var3){
+function controleIF($var1, $var2, $var3)
+{
 
-    if ($var1 > $var2 && $var3 == 0){
+    if ($var1 > $var2 && $var3 == 0) {
         echo "True";
-    }else{
+    } else {
         echo "False";
     }
 
-    if ($var1 > $var2 && $var3 == 0){
+    if ($var1 > $var2 && $var3 == 0) {
         echo "True";
-    }else if ($var2 == 2) {
+    } else if ($var2 == 2) {
         echo "Else If";
     }
 
-    if ($var1 > $var2 ){
-        if($var3 == 0){
+    if ($var1 > $var2) {
+        if ($var3 == 0) {
             echo "True IF IF";
         }
-     }
+    }
 }
