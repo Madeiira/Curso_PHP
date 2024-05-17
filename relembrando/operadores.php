f9cc15c8a57444821c9164a19d2f690b9ee8800c
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-16 23:47:34 -0300

    Adicionando operadores lógicos

--- relembrando/operadores.php
+++ relembrando/operadores.php
@@ -20,6 +20,7 @@ function contaDefault($a, $b, $c)
 contaDefault(2, 2, 6);
 
 echo "<br>";
+echo "<br>";
 
 function contaDif($a, $b, $c)
 {
@@ -30,6 +31,7 @@ function contaDif($a, $b, $c)
 
 contaDif(2, 2, 6);
 echo "<br>";
+echo "<br>";
 
 //AUTOCAST
 
@@ -38,6 +40,8 @@ function autoCast($var1, $var2)
 
     echo $divisao = $var1 / $var2; //ceil() para arredondar para cima
     echo "<br>";
+    echo "<br>";
+
 }
 
 
@@ -51,6 +55,8 @@ function getTypee($var1, $var2)
 
     echo "gettype: " . gettype($expressao);
     echo "<br>";
+    echo "<br>";
+
 }
 
 getTypee("5", 12);
@@ -62,11 +68,16 @@ getTypee("5", 12);
 function operadorModulo($var1, $var2)
 {
 
-    echo "Modulo: " . $modulo = $var1 % $var2;
-    echo "<br>";
+    echo "Modulo: " . $modulo = $var1 % $var2 ." (".$var1." e ".$var2.")";
+
+
 }
 
 operadorModulo(6, 2);
+echo "<br>";
+operadorModulo(5, 2);
+echo "<br>";
+echo "<br>";
 
 //Operador de exponenciação
 
@@ -75,7 +86,146 @@ function operadorExponenciacao($var1, $var2)
 
     echo "Exponenciação: " . $exponenciacao = $var1 ** $var2;
     echo "<br>";
+    echo "<br>";
 
 }
 
 operadorExponenciacao(5, 2);
+
+//Operadores de comparação
+
+/*
+Igualdade    == 
+Idêntico a   ===
+Diferente    !=
+Não idêntico !==
+Maior        >
+Maior igual  >=
+Menor        <
+Menor igual  <=
+*/
+
+
+function igualdade($var1, $var2)
+{
+
+
+    if ($var1 == $var2) {
+        echo "Verdadeiro Igualdade";
+        echo "<br>";
+    } else {
+        echo "Falso Igualdade";
+        echo "<br>";
+    }
+}
+
+igualdade(2, 2);
+
+igualdade(2, 1);
+echo "<br>";
+
+function identico($var1, $var2)
+{
+    if ($var1 === $var2) {
+        echo "Verdadeiro Idêntico";
+        echo "<br>";
+    }
+}
+
+identico(2, "2");
+
+identico(2, 1);
+echo "<br>";
+
+function diferente($var1, $var2)
+{
+    if ($var1 != $var2) {
+        echo "Verdadeiro diferente";
+        echo "<br>";
+    } else {
+        echo "Falso diferente";
+        echo "<br>";
+    }
+}
+
+diferente(2, "2");
+
+diferente(2, 1);
+echo "<br>";
+
+function naoIdentico($var1, $var2)
+{
+    if ($var1 !== $var2) {
+        echo "Verdadeiro não idêntico";
+        echo "<br>";
+    }
+}
+
+identico(2, "2");
+
+identico(2, 1);
+echo "<br>";
+
+function maior($var1, $var2)
+{
+    if ($var1 > $var2) {
+        echo "Verdadeiro Maior";
+        echo "<br>";
+    } else {
+        echo "Falso Maior";
+        echo "<br>";
+    }
+}
+
+maior(2, 2);
+
+maior(2, 1);
+echo "<br>";
+
+function maiorIgual($var1, $var2)
+{
+    if ($var1 >= $var2) {
+        echo "Verdadeiro Maior igual";
+        echo "<br>";
+    } else {
+        echo "Falso Maior Igual";
+        echo "<br>";
+    }
+}
+
+maiorIgual(2, 2);
+
+maiorIgual(2, 1);
+echo "<br>";
+
+function menor($var1, $var2)
+{
+    if ($var1 < $var2) {
+        echo "Verdadeiro menor igual";
+        echo "<br>";
+    } else {
+        echo "Falso menor Igual";
+        echo "<br>";
+    }
+}
+
+menor(2, 2);
+
+menor(2, 1);
+echo "<br>";
+
+function menorIgual($var1, $var2)
+{
+    if ($var1 >= $var2) {
+        echo "Verdadeiro menor igual";
+        echo "<br>";
+    } else {
+        echo "Falso menor Igual";
+        echo "<br>";
+    }
+}
+
+menorIgual(2, 2);
+
+menorIgual(2, 1);
+echo "<br>";
