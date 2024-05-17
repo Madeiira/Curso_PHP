6a72677433c27b115551b6687ca03540adb77524
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-16 23:24:48 -0300

    Formatando código

--- relembrando/operadores.php
+++ relembrando/operadores.php
@@ -10,72 +10,72 @@
 // Ordem dos operadores é cfe a matematica
 // 2 + 2 * 6 = 14
 
-function contaDefault($a, $b, $c){
+function contaDefault($a, $b, $c)
+{
 
     $contaDefault = $a + $b * $c;
-    echo "Conta default: ". $contaDefault;
-
+    echo "Conta default: " . $contaDefault;
 }
 
-contaDefault(2,2,6);
+contaDefault(2, 2, 6);
 
 echo "<br>";
 
-function contaDif($a, $b, $c){
+function contaDif($a, $b, $c)
+{
 
     $contaDif = ($a + $b) * $c;
-    echo "Conta dif: ". $contaDif;
-
+    echo "Conta dif: " . $contaDif;
 }
 
-contaDif(2,2,6);
+contaDif(2, 2, 6);
 echo "<br>";
 
 //AUTOCAST
 
-function autoCast($var1,$var2){
+function autoCast($var1, $var2)
+{
 
     echo $divisao = $var1 / $var2; //ceil() para arredondar para cima
     echo "<br>";
-
 }
 
 
-autoCast(5,2);
+autoCast(5, 2);
 
-function getTypee($var1,$var2){
+function getTypee($var1, $var2)
+{
 
     echo $expressao = $var1 * $var2;
     echo "<br>";
 
-    echo "gettype: ". gettype($expressao);
+    echo "gettype: " . gettype($expressao);
     echo "<br>";
-
 }
 
-getTypee("5",12);
+getTypee("5", 12);
 
 
 
 //OPERADOR DE MODULO
 
-function operadorModulo($var1,$var2){
+function operadorModulo($var1, $var2)
+{
 
-    echo "Modulo: ". $modulo = $var1 % $var2;
+    echo "Modulo: " . $modulo = $var1 % $var2;
     echo "<br>";
-
 }
 
-operadorModulo(6,2);
+operadorModulo(6, 2);
 
 //Operador de exponenciação
 
-function operadorExponenciacao($var1,$var2){
+function operadorExponenciacao($var1, $var2)
+{
 
-
-    echo "Exponenciação: ". $exponenciacao = $var1 ** $var2;
+    echo "Exponenciação: " . $exponenciacao = $var1 ** $var2;
     echo "<br>";
 
 }
 
-operadorExponenciacao(5,2);
+operadorExponenciacao(5, 2);
