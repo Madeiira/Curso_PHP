ca334395bd48324c4a47acb0f46b7e223ee1a42a
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-17 17:57:54 -0300

    Adicionando else if

--- relembrando/estruturaControle.php
+++ relembrando/estruturaControle.php
@@ -8,4 +8,10 @@ function controleIF($var1, $var2, $var3){
         echo "False";
     }
 
+    if ($var1 > $var2 && $var3 == 0){
+        echo "True";
+    }else if ($var2 == 2) {
+        echo "Else If";
+    }
+
 }
