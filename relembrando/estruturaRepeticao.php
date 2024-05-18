2849261afeb6667aa98008920da89877f18af198
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-17 22:59:16 -0300

    Adicioanndo exemplo While

--- relembrando/estruturaRepeticao.php
+++ relembrando/estruturaRepeticao.php
@@ -1,1 +1,17 @@
 <?php
+
+// Estrutura While
+
+function estruturaWhile($var1){
+
+
+    while($var1 <= 10){
+        if ($var1 % 2 !=0)
+        echo "Impares: ".$var1; 
+        echo "<br>";
+        $var1++;
+    }
+
+}
+
+estruturaWhile(0);
