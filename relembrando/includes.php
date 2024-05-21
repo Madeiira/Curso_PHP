06aaed40eb8b6456418af4e06abcf356798e35c6
Gabriel Anjos <gmmanjos@gmail.com>
2024-05-21 14:52:36 -0300

    Relembrando - adicionando comentários referente ao método include

--- relembrando/includes.php
+++ relembrando/includes.php
@@ -1,9 +1,20 @@
 <?php
 
-// Iniciando Relembrando Includes
+//  Iniciando Relembrando Includes
 
-include "includes/exemplo.php";
+// include "includes/exemplo.php";
 
-echo "<p> após o include </p> <br> $var1";
+// echo "<p> após o include </p> <br> $var1"; 
+
+// INCLUDE NAO GERA ERRO FATAL APENAS WARNING
+// para nao notificar o warning 
+// encontre o php.ini
+// Localize no arquivo o seguinte código: error_reporting = E_ALL
+// Substitua por: error_reporting = E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED
+// Salve as alterações e reinicie o PHP + APACHE.
+
+// PARA WORDPRESS TEM OUTRA FORMA
+
+
 ?>
 
