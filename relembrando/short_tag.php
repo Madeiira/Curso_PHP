16cc4b35d0602690a12acfb8a4c0e370de916f58
Gabriel Anjos <gmmanjos@gmail.com>
2024-06-06 12:12:57 -0300

    Relembrando - Finalizando comentários

--- relembrando/short_tag.php
+++ relembrando/short_tag.php
@@ -1,4 +1,4 @@
 <?php
 
 // Forma de incluir codigo PHP dentro de uma pagina 
-// Depende de uma configuração do servidor, logo não é tão recomendada 
+// Depende de uma configuração do servidor, logo não é tão recomendada sem a abertura e fechamento da tag php
